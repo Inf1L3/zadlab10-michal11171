c0bfff2f0788d51d047d6e050f38c9799d4c02bc
Michal <michal11171@gmail.com>
2018-05-16 17:15:04 +0200

    Zadania z lab

--- OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
@@ -7,7 +7,7 @@ package pl.edu.ur.oopl10;
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
     
 }
---
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/Zadanie2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl10;
+import java.util.Scanner;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int[] tablica = {1, 2, 3, 4, 5, 6, 7};
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ktory element tablicy wyświetlic?");
+        int index = scanner.nextInt();
+        scanner.close();
+        try {
+            System.out.println(tablica[index]);
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.err.println("Podano indeks wykraczajacy poza rozmiar tablicy");
+        }
+    }
+}
